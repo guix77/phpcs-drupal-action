91d6bf1c27110b45049c43bd40687bd7bdfa4263
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-06-27 03:51:53 +0200

    up

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,6 +4,7 @@ cp /app/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
+ls -la
 phpcs --standard=/root/.composer/vendor/drupal/coder/coder_sniffer/Drupal,/root/.composer/vendor/drupal/coder/coder_sniffer/DrupalPractice web/modules/custom web/themes/custom
 
 status=$?
