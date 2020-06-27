55765b5212913d879b2b36db7463057dc7c8a968
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-06-27 03:33:26 +0200

    up

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cp /app/problem-matcher.json /github/workflow/problem-matcher.json
+cp /app/ruleset.xml /github/workflow/ruleset.xml
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
-phpcs  --standard=/app/ruleset.xml
+phpcs  --standard=${RUNNER_TEMP}/_github_workflow/ruleset.xml
 
 status=$?
 
