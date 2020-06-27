025e243a46636b42dfa0c7a54b354631f2c87b1e
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-06-27 02:16:05 +0200

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ cp /app/action/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
+${INPUT_PHPCS_BIN_PATH} --report=checkstyle
+
 status=$?
 
 echo "::remove-matcher owner=phpcs::"
