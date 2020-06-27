81b77958f0dd14e8d7bd9a96f6ce15edba60d66a
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-06-27 04:50:11 +0200

    up

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,7 +4,7 @@ cp /app/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
-phpcs --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml web/modules/custom web/themes/custom
+phpcs
 
 status=$?
 
